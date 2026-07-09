671b011010d04383a09bcf1e4dc64f2699ff3e98
vitorspada <vitorspada.rs@gmail.com>
2026-07-09 17:03:10 -0300

    criação da função de load

--- etl.py
+++ etl.py
@@ -15,11 +15,19 @@ def calcular_kpi_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
     df["Total"] = df["Quantidade"] * df["Venda"]
     return df
 
-
 # Load
+
+def carregar_dados(df: pd.DataFrame, formato_saida: list):
+    for formato in formato_saida :
+        if formato == 'csv':
+            df.to_csv("dados.csv", index=False)
+        if formato == 'parquet':
+            df.to_parquet("dados.parquet", index=False)
 
 
 if __name__ == "__main__":
-    pasta = 'data'
+    pasta: str = 'data'
     data_frame = extrair_dados_e_consolidar(pasta)
-    print(calcular_kpi_de_vendas(data_frame))
+    data_frame_calculado = calcular_kpi_de_vendas(data_frame)
+    formatos_de_saida = ["csv", "parquet"]
+    carregar_dados(data_frame_calculado, formatos_de_saida)
